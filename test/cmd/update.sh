b34aabbbd9ee1c81cbcb8d071d2ec6804375770b
mijime <07cc0110+gthb@gmail.com>
2016-04-06 09:25:30 +0900

    :art: Add test for update command

---
+++ test/cmd/update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+setup() {
+  unset SHAM_PLUGS;
+  export SHAM_HOME="/tmp/sham/${UNITTEST_NO}";
+  [[ ! -d "${SHAM_HOME}" ]] || rm -r "${SHAM_HOME}";
+
+  source bin/sham.sh;
+}
+
+__test__update_01 () {
+  sham mijime/sham --from=file://. --at=HEAD^^;
+  sham install;
+
+  sham mijime/sham --from=file://.;
+  sham update;
+
+  [[ -d "${SHAM_HOME}/repos/mijime/sham" ]];
+}
+
+__test__update_02_noinstall () {
+  sham mijime/sham --from=file://.;
+  sham update;
+
+  [[ ! -d "${SHAM_HOME}/repos/mijime/sham" ]];
+}

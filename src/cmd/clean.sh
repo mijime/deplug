11dde62cccfab5b1a23f8b567b9520439bc9fdf5
mijime <07cc0110+gthb@gmail.com>
2016-04-06 10:23:42 +0900

    :art: Add clean command

---
+++ src/cmd/clean.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+__sham__cmd__clean() {
+  local __v__tmp=
+
+  __sham__util__plugs \
+    | while read __v__tmp
+      do
+        __sham__util__parse
+
+        case ${__v__stat} in
+          0|1|2)
+            __sham__util__stringify
+            continue
+            ;;
+        esac
+
+        if [[ -d "${__v__dir}" ]]
+        then
+          rm -rf "${__v__dir}"
+        fi
+
+      done > "${__g__state}".tmp
+  mv "${__g__state}"{.tmp,}
+
+  if [[ -f "${__g__cache}".tmp ]]
+  then
+    mv "${__g__cache}"{.tmp,}
+  fi
+
+  unset SHAM_PLUGS
+}
---
+++ test/cmd/clean.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+setup() {
+  unset SHAM_PLUGS;
+  export SHAM_HOME="/tmp/sham/${UNITTEST_NO}";
+  [[ ! -d "${SHAM_HOME}" ]] || rm -r "${SHAM_HOME}";
+  source bin/sham.sh;
+}
+
+teardown() {
+  unset SHAM_PLUGS;
+  export SHAM_HOME="/tmp/sham/${UNITTEST_NO}";
+  [[ ! -d "${SHAM_HOME}" ]] || rm -r "${SHAM_HOME}";
+}
+
+__test__clean_01 () {
+  sham mijime/sham00 --from=file://.;
+  sham mijime/sham01 --from=file://.;
+  sham install;
+
+  sham mijime/sham01 --from=file://.;
+  sham install;
+
+  sham clean;
+
+  [[ ! -d "${SHAM_HOME}/repos/mijime/sham00" ]] \
+    && [[ -d "${SHAM_HOME}/repos/mijime/sham01" ]];
+}

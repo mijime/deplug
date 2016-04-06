c4323841625fad633ee008e6131fcbce126018c1
mijime <07cc0110+gthb@gmail.com>
2016-04-06 09:22:33 +0900

    :art: Add repository for github

---
+++ src/repo/github.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+__sham__repo__file() {
+  case "${__g__cmd}" in
+    install)
+      if [[ ! -d "${__v__dir}" ]] && ! git clone "https://github.com/${__v__from#*://}" "${__v__dir}" >&2
+      then
+        echo 4
+        return 1
+      fi
+
+      local __v__curr_dir=$(pwd)
+
+      cd "${__v__dir}"
+
+      if [[ ! -z "${__v__at}" ]] && ! git checkout "${__v__at}" >&2
+      then
+        cd "${__v__curr_dir}"
+
+        echo 4
+        return 1
+      fi
+
+      cd "${__v__curr_dir}"
+      ;;
+
+    update)
+      if [[ ! -d "${__v__dir}" ]]
+      then
+        echo 1
+        return 1
+      fi
+
+      local __v__curr_dir=$(pwd)
+
+      cd "${__v__dir}"
+
+      if ! git fetch >&2
+      then
+        cd "${__v__curr_dir}"
+
+        echo 4
+        return 1
+      fi
+
+      if [[ ! -z "${__v__at}" ]] && ! git checkout "${__v__at}" >&2
+      then
+        cd "${__v__curr_dir}"
+
+        echo 4
+        return 1
+      fi
+
+      if ! git pull >&2
+      then
+        cd "${__v__curr_dir}"
+
+        echo 4
+        return 1
+      fi
+
+      cd "${__v__curr_dir}"
+      ;;
+
+    *)
+      ;;
+  esac
+
+  echo 0
+  return
+}
